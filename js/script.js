b9c45eef52904c9fd22d34c497e999646e25501c
santos <santos.sanchez@bue.edu.ar>
2022-12-07 21:07:30 -0300

    proyecto final modulo 2

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+function  showInput() {
+    document.getElementById("inputAcercaDe").style.display="block";
+}
+
+function updAcercaDe() {
+    document.getElementById("acercaDe").innerHTML=document.getElementById("inputAcercaDe").value;
+}
